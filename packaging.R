b06b3f8dde7b36c758fb3ad8897a3e01c3aa3af2
MMA <MMA@nbmma2>
2018-04-23 13:23:32 +0200

    added verbose argument and release of missRanger 1.0.2

--- packaging.R
+++ packaging.R
@@ -11,7 +11,7 @@ unlink(pkg, force = TRUE, recursive = TRUE)
 create(pkg, descr = list(
             Title = "Fast Imputation of Missing Values",
             Type = "Package",
-            Version = "1.0.1",
+            Version = "1.0.2",
             Date = Sys.Date(),
             Description = "Alternative implementation of the beautiful 'MissForest' algorithm used to impute mixed-type data sets by chaining tree ensembles, introduced by Stekhoven, D.J. and Buehlmann, P. (2012) <doi:10.1093/bioinformatics/btr597>. Under the hood, it uses the lightning fast random jungle package 'ranger'. Between the iterative model fitting, we offer the option of using predictive mean matching. This firstly avoids imputation with values not already present in the original data (like a value 0.3334 in 0-1 coded variable). Secondly, predictive mean matching tries to raise the variance in the resulting conditional distributions to a realistic level. This would allow e.g. to do multiple imputation when repeating the call to missRanger().",
             
@@ -32,7 +32,7 @@ file.copy(fp, file.path(pkg, "R"))
 document(pkg)
 
 # Add further files
-# devtools::use_cran_comments(pkg)
+# devtools::use_cran_comments(pkg) (is required)
 mdfiles <- c("NEWS.md", "README.md")
 stopifnot(file.exists(mdfiles))
 file.copy(mdfiles, pkg)
@@ -55,7 +55,7 @@ if (FALSE) {
   library(missRanger)
   
   irisWithNA <- generateNA(iris)
-  irisImputed <- missRanger(irisWithNA, pmm.k = 3, num.trees = 100)
+  irisImputed <- missRanger(irisWithNA, pmm.k = 3, num.trees = 100, verbose = 1)
   head(irisImputed)
   head(irisWithNA)
   head(iris)
