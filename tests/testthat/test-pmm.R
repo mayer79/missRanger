7c17108d2df4115dce5db4af4c0b162cb8752c68
Michael Mayer <mayermichael79@gmail.com>
2023-03-22 20:31:55 +0100

    more tests and explicit integers

--- tests/testthat/test-pmm.R
+++ tests/testthat/test-pmm.R
@@ -1,39 +1,30 @@
 test_that("pmm picks the right reference based on numeric values", {
   expect_equal(
-    pmm(xtrain = c(0.2, 0.2, 0.8), xtest = 0.3, ytrain = c(0, 0, 1)), 
-    0
+    pmm(xtrain = c(0.2, 0.2, 0.8), xtest = 0.3, ytrain = c(0, 0, 1)), 0
   )
   expect_equal(pmm(1:10, 3:4, 1:10), 3:4)
 })
 
 test_that("pmm picks the right reference based on logical values", {
   expect_equal(
-    pmm(xtrain = c(TRUE, FALSE, TRUE), xtest = FALSE, ytrain = c(2, 0, 1)), 
-    0
+    pmm(xtrain = c(TRUE, FALSE, TRUE), xtest = FALSE, ytrain = c(2, 0, 1)), 0
   )
 })
 
 test_that("pmm picks the right reference based on character values", {
   expect_equal(
-    pmm(xtrain = c("A", "A", "B"), xtest = "A", ytrain = c(2, 2, 4), k = 2), 
-    2
+    pmm(xtrain = c("A", "A", "B"), xtest = "A", ytrain = c(2, 2, 4), k = 2), 2
   )
 })
 
 test_that("pmm picks the right reference based on a factor", {
-  expect_equal(
-    pmm(xtrain = factor(c("A", "B")), xtest = factor("C"), ytrain = 1:2), 
-    2
-  )
+  expect_equal(pmm(xtrain = factor(c("A", "B")), xtest = factor("C"), ytrain = 1:2), 2)
 })
 
 test_that("pmm picks the right reference based on date vector", {
   xtrain <- as.Date(c("2018-01-01", "2018-01-02"))
   xquery <- as.Date(c("2018-01-03", "2018-01-04"))
-  expect_equal(
-    pmm(xtrain, xquery, ytrain = 1:2), 
-    rep(2, 2)
-  )
+  expect_equal(pmm(xtrain, xquery, ytrain = 1:2), rep(2, 2))
 })
 
 test_that("pmm gives error if ytrain is of wrong length", {
@@ -41,38 +32,23 @@ test_that("pmm gives error if ytrain is of wrong length", {
 })
 
 test_that("pmm works for logical y", {
-  expect_equal(
-    pmm(1, xtest = Sys.Date(), ytrain = TRUE), 
-    TRUE
-  )
+  expect_equal(pmm(1, xtest = Sys.Date(), ytrain = TRUE), TRUE)
 })
 
 test_that("pmm works for factor y", {
-  expect_equal(
-    pmm(1, xtest = Sys.Date(), ytrain = factor("A")), 
-    factor("A")
-  )
+  expect_equal(pmm(1, xtest = Sys.Date(), ytrain = factor("A")), factor("A"))
 })
 
 test_that("pmm works for date y", {
-  expect_equal(
-    pmm(1, xtest = Sys.Date(), ytrain = Sys.Date()), 
-    Sys.Date()
-  )
+  expect_equal(pmm(1, xtest = Sys.Date(), ytrain = Sys.Date()), Sys.Date())
 })
 
 test_that("pmm works with NA in xtrain", {
-  expect_equal(
-    pmm(c(NA, 1, 2), 1, ytrain = c(0, 1, NA)), 
-    1
-  )
+  expect_equal(pmm(c(NA, 1, 2), 1, ytrain = c(0, 1, NA)), 1)
 })
 
 test_that("pmm works with NA in xtrain and NA in ytrain", {
-  expect_equal(
-    pmm(c(NA, 1, 2), 1, ytrain = c(0, NA, 3)), 
-    3
-  )
+  expect_equal(pmm(c(NA, 1, 2), 1, ytrain = c(0, NA, 3)), 3)
 })
 
 test_that("pmm gives error when xtest contains NA", {
