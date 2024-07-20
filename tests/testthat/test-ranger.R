79175814d93a03e59421a6adc34afcb51266f5b9
Michael Mayer <mayermichael79@gmail.com>
2024-07-20 23:14:51 +0200

    Slightly better tests for ranger()

--- tests/testthat/test-ranger.R
+++ tests/testthat/test-ranger.R
@@ -1,7 +1,7 @@
 # STABILITY OF COVARIATES
 
 test_that("x/y API of ranger does not care about feature order in x at prediction", {
-  fit <- ranger::ranger(y = iris[[1L]], x = iris[, 2:5], num.trees = 40L)
+  fit <- ranger::ranger(y = iris[[1L]], x = iris[, 2:5], num.trees = 10L)
   
   pred1 <- predict(fit, iris[1:5, 2:5])
   pred2 <- predict(fit, iris[1:5, 5:2])
@@ -9,26 +9,30 @@ test_that("x/y API of ranger does not care about feature order in x at predictio
 })
 
 test_that("x/y API of ranger does not care about extra columns during prediction", {
-  fit <- ranger::ranger(y = iris[[1L]], x = iris[, 2:5], num.trees = 40L)
+  fit <- ranger::ranger(y = iris[[1L]], x = iris[, 2:5], num.trees = 10L)
   
   pred1 <- predict(fit, iris[1:5, 2:5])
   pred2 <- predict(fit, iris[1:5, ])
   expect_equal(pred1, pred2)
 })
 
-test_that("ranger can safely predict on character feature, even for unseen values or if factor", {
+test_that("ranger can flexibly predict on feature being character at fit time", {
   ir <- transform(iris, Species = as.character(Species))
+  
+  # Fit on character
   fit <- ranger::ranger(y = ir[[1L]], x = ir["Species"], num.trees = 10L)
   
+  # Predict on character
   pred1 <- predict(fit, ir[c(1L, 51L, 101L), ])$predictions
   
+  # Predict on data with just one row
   pred2 <- c(
     predict(fit, ir[1L, ])$predictions,
     predict(fit, ir[51L, ])$predictions,
     predict(fit, ir[101L, ])$predictions
   )
 
-  # At prediction time, feature is factor
+  # Predict on factor (iris instead of ir)
   pred3 <- predict(fit, iris[c(1L, 51L, 101L), ])$predictions
   
   expect_equal(pred1, pred2)
@@ -37,9 +41,11 @@ test_that("ranger can safely predict on character feature, even for unseen value
   expect_no_error(predict(fit, transform(ir[1:2, ], Species = factor("new value"))))
 })
 
-test_that("ranger safely predicts on factor feature, even with inconsistent levels or if character", {
+test_that("ranger can flexibly predict on feature being factor at fit time", {
+  # Fit on factor
   fit <- ranger::ranger(y = iris[[1L]], x = iris[5L], num.trees = 10L)
   
+  # Predict on factor with consistent levels
   pred1 <- predict(fit, iris[c(1L, 51L, 101L), ])$predictions
   
   # Incomplete factor levels at prediction time
@@ -65,48 +71,15 @@ test_that("ranger safely predicts on factor feature, even with inconsistent leve
   expect_no_error(predict(fit, transform(iris[1L, ], Species = "new value")))
 })
 
-test_that("storage mode of binary feature does not matter", {
+test_that("ranger does not care if integer is represented as double", {
   y <- iris$Sepal.Length
-  x1 <- transform(iris[4:5], Species = Species == "setosa")
-  x2 <- transform(iris[4:5], Species = 1L * (Species == "setosa"))
-  x3 <- transform(iris[4:5], Species = 1.0 * (Species == "setosa"))
-  
-  fit1 <- ranger::ranger(y = y, x = x1, num.trees = 20L, seed = 1L)
-  fit2 <- ranger::ranger(y = y, x = x2, num.trees = 20L, seed = 1L)
-  fit3 <- ranger::ranger(y = y, x = x3, num.trees = 20L, seed = 1L)
-  
-  # Compare models
-  expect_equal(
-    predict(fit1, x1[1:5, ])$predictions,
-    predict(fit2, x2[1:5, ])$predictions
-  )
-  expect_equal(
-    predict(fit1, x1[1:5, ])$predictions,
-    predict(fit3, x3[1:5, ])$predictions
-  )
-  
-  # Compare predictions for one model applied to "wrong" data type
-  for (model in list(fit1, fit2, fit3)) {
-    expect_equal(
-      predict(model, x1[1:5, ])$predictions,
-      predict(model, x2[1:5, ])$predictions
-    )
-    expect_equal(
-      predict(model, x1[1:5, ])$predictions,
-      predict(model, x3[1:5, ])$predictions
-    )
-  }
-})
-
-test_that("integer feature can also be double", {
-  y <- iris$Sepal.Length
-  x1 <- round(iris[2L])
+  x1 <- round(iris["Sepal.Width"])  # Double
   x2 <- transform(x1, Sepal.Width = as.integer(Sepal.Width))
   
-  fit1 <- ranger::ranger(y = y, x = x1, num.trees = 20L, seed = 1L)
-  fit2 <- ranger::ranger(y = y, x = x2, num.trees = 20L, seed = 1L)
+  fit1 <- ranger::ranger(y = y, x = x1, num.trees = 10L, seed = 1L)
+  fit2 <- ranger::ranger(y = y, x = x2, num.trees = 10L, seed = 1L)
   
-  # Compare models
+  # Compare predictions on "native" data type
   expect_equal(
     predict(fit1, x1[1:5, , drop = FALSE])$predictions,
     predict(fit2, x2[1:5, , drop = FALSE])$predictions
@@ -121,15 +94,38 @@ test_that("integer feature can also be double", {
   }
 })
 
-test_that("date feature can be represented as number for identical results", {
+test_that("ranger does not care if logical is represented as double", {
+  y <- iris$Sepal.Length
+  x1 <- iris["Sepal.Width"] > median(iris$Sepal.Width)
+  x2 <- transform(x1, Sepal.Width = as.double(Sepal.Width))
+  
+  fit1 <- ranger::ranger(y = y, x = x1, num.trees = 10L, seed = 1L)
+  fit2 <- ranger::ranger(y = y, x = x2, num.trees = 10L, seed = 1L)
+  
+  # Compare predictions on "native" data type
+  expect_equal(
+    predict(fit1, x1[1:5, , drop = FALSE])$predictions,
+    predict(fit2, x2[1:5, , drop = FALSE])$predictions
+  )
+  
+  # Compare predictions for one model applied to "wrong" data type
+  for (model in list(fit1, fit2)) {
+    expect_equal(
+      predict(model, x1[1:5, , drop = FALSE])$predictions,
+      predict(model, x2[1:5, , drop = FALSE])$predictions
+    )
+  }
+})
+
+test_that("date feature work (and can be represented as number)", {
   y <- seq(-1, 1, length.out = 100)^2
   x1 <- data.frame(time = seq.Date(as.Date("2024-01-01"), by = "1 d", length.out = 100))
   x2 <- transform(x1, time = as.numeric(time))
   
-  fit1 <- ranger::ranger(y = y, x = x1, num.trees = 20L, seed = 1L)
-  fit2 <- ranger::ranger(y = y, x = x2, num.trees = 20L, seed = 1L)
+  fit1 <- ranger::ranger(y = y, x = x1, num.trees = 10L, seed = 1L)
+  fit2 <- ranger::ranger(y = y, x = x2, num.trees = 10L, seed = 1L)
   
-  # Compare models
+  # Compare predictions
   expect_equal(
     predict(fit1, x1[1:5, , drop = FALSE])$predictions,
     predict(fit2, x2[1:5, , drop = FALSE])$predictions
@@ -150,7 +146,7 @@ test_that("ranger works with logical responses, but predictions are 0 or 1", {
   y <- iris$Sepal.Length > median(iris$Sepal.Length)
   x <- iris[2:4]
   
-  fit <- ranger::ranger(y = y, x = x, num.trees = 100L)
+  fit <- ranger::ranger(y = y, x = x, num.trees = 50L, seed = 1L)
   pred <- predict(fit, iris)$predictions
   
   expect_equal(fit$treetype, "Classification")
@@ -173,7 +169,7 @@ test_that("factor responses result in factor predictions with right levels", {
 })
 
 test_that("character responses are not accepted", {
-  # Shows that character responses has to be converted to character
+  # Shows that character responses have to be converted to character
   y <- as.character(iris$Species)
   x <- iris[2:4]
   
